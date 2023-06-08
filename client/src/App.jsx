32052d1325ce64042aa857f15b7f01275b789f01
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-06-08 16:04:12 +0530

    fixed errors of routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import { buildRoutes } from "./utilities";
+import { routes } from "./router/routes";
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages";
+import { PATHS } from "./router/paths";
+
 const App = () => {
-  return <div className="font-body"> App</div>;
+  return (
+    <div className="font-body">
+      <Routes>
+        {buildRoutes(routes)}
+        <Route element={<HomePage />} path={PATHS.home} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- server/controllers/astrolok.js
+++ server/controllers/astrolok.js
@@ -6,82 +6,83 @@ const jwt = require("jsonwebtoken");
 
 //login
 exports.login = async (req, res) => {
+  try {
+    //collected information from frontend
+    const { email, password } = req.body;
 
-    try {
-        //collected information from frontend
-        const { email, password } = req.body
-         
-        //validate
-        if (!email || !password) {
-            return res.status(404).json({
-                success: false,
-                message: "please send email or password"
-            })
-        }
+    //validate
+    if (!email || !password) {
+      return res.status(404).json({
+        success: false,
+        message: "please send email or password",
+      });
+    } else {
+      const userExist = await Usermodel.findOne({ email });
 
-        else {
-
-            const userExist = await Usermodel.findOne({ email })
-          
-            if (userExist) {
-                if (!(await bcrypt.compare(password, userExist.password))){
-                   return res.status(400).json({
-                        success:false,
-                        message:"password is incorrect"
-                    })
-                }
-
-                     token = jwt.sign({ id: userExist._id, role: userExist.role }, process.env.SECRET_KEY, { expiresIn: '2h' })
-                     userExist.password = undefined
-                // userDetails.token = token
-
-                const options = {
-                    // i am currently removing domain as in postman it is not creating token in cookies
-                    // domain: process.env.REACT_APP_URL,
-                    expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-                    httpOnly: true
-                }
-                return res.status(200).cookie("token", token, options).json({
-                    success: true,
-                    token,
-                    id: userExist._id, role: userExist.role 
-                })
-
-            }
-            const astroExist = await AstrologerModel.findOne({ email })
-            if (astroExist) {
-                if (!await bcrypt.compare(password, astroExist.password)){
-                   return res.status(400).json({
-                        success:false,
-                        message:"password is incorrect"
-                    })
-                }
-
-
-                     token = jwt.sign({ id: astroExist._id, role:astroExist.role }, process.env.SECRET_KEY, { expiresIn: '2h' })
-                     astroExist.password = undefined
-                // userDetails.token = token
-
-                const options = {
-                    // i am currently removing domain as in postman it is not creating token in cookies
-                    // domain: process.env.REACT_APP_URL,
-                    expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-                    httpOnly: true
-                }
-                return res.status(200).cookie("token", token, options).json({
-                    success: true,
-                    token,
-                    id: astroExist._id, role:astroExist.role
-                })
-
-            }
-
-        }
-    } catch (error) {
-        return res.status(500).json({
+      if (userExist) {
+        if (!(await bcrypt.compare(password, userExist.password))) {
+          return res.status(400).json({
             success: false,
-            message: error.message
-        })
-    }
+            message: "password is incorrect",
+          });
+        }
 
+        token = jwt.sign(
+          { id: userExist._id, role: userExist.role },
+          process.env.SECRET_KEY,
+          { expiresIn: "2h" }
+        );
+        userExist.password = undefined;
+        // userDetails.token = token
+
+        const options = {
+          // i am currently removing domain as in postman it is not creating token in cookies
+          // domain: process.env.REACT_APP_URL,
+          expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        };
+        return res.status(200).cookie("token", token, options).json({
+          success: true,
+          token,
+          id: userExist._id,
+          role: userExist.role,
+        });
+      }
+      const astroExist = await AstrologerModel.findOne({ email });
+      if (astroExist) {
+        if (!(await bcrypt.compare(password, astroExist.password))) {
+          return res.status(400).json({
+            success: false,
+            message: "password is incorrect",
+          });
+        }
+
+        token = jwt.sign(
+          { id: astroExist._id, role: astroExist.role },
+          process.env.SECRET_KEY,
+          { expiresIn: "2h" }
+        );
+        astroExist.password = undefined;
+        // userDetails.token = token
+
+        const options = {
+          // i am currently removing domain as in postman it is not creating token in cookies
+          // domain: process.env.REACT_APP_URL,
+          expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        };
+        return res.status(200).cookie("token", token, options).json({
+          success: true,
+          token,
+          id: astroExist._id,
+          role: astroExist.role,
+        });
+      }
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
