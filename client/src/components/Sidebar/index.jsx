d43e88538c32b76b9b5d11bf966db38c9555d541
nauts06 <nautscode@gmail.com>
2023-06-10 15:09:53 +0530

    added changes to sidebar

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -1,38 +1,229 @@
-import {routes}  from "../../router/routes";
+import { useState } from "react";
 import { toggleSidebar } from "../../features/appConfig/AppSlice";
 import { useDispatch, useSelector } from "react-redux";
 const Sidebar = () => {
+  const [selectedOption, setSelectedOption] = useState("Dashboard");
   const dispatch = useDispatch();
   const sidebarToggle = useSelector((x) => x.appConfig.sidebarOpen);
   const handleClick = () => {
     dispatch(toggleSidebar());
   };
 
-<<<<<<< HEAD
-=======
-    //  console.log("sidebarToggle", routes);
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
 
-  // className={`bg-red-500 ${isActive ? 'text-white' : 'text-gray-500'}`}sm:w-[920px]
->>>>>>> 3e34ad6922b9a312be177c466be6a907301aa435
+  const routes = [
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+          />
+        </svg>
+      ),
+      name: "Dashboard",
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+          />
+        </svg>
+      ),
+      name: "Birth Details",
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+          />
+        </svg>
+      ),
+      name: "Consultant List",
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+          />
+        </svg>
+      ),
+      name: "Course List",
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+          />
+        </svg>
+      ),
+      name: "User Profile",
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+          />
+        </svg>
+      ),
+      name: "User Reports",
+    },
+  ];
+
   return (
-    <div
-      className={`duration-300 ease-in-out bg-gray-500 ${
-        sidebarToggle === true
-          ? "h-screen w-[20%] sm:w-640px md:w-768px lg:w-1024px"
-          : "h-screen w-[7%] sm:w-640px md:w-768px lg:w-1024px"
-      }`}
-    >
-<<<<<<< HEAD
-      <div onClick={handleClick}>side</div>
-=======
-      <div onClick={handleClick} className="cursor-pointer">
-        side
-      </div>
+    <div className="flex h-screen">
+      <div
+        className={`bg-gray-200 flex flex-col items-center justify-center"  duration-300 ease-in-out  ${
+          sidebarToggle === true
+            ? "h-screen sm: w-[50%] lg:w-[20%]"
+            : "h-screen w-[7%] "
+        }`}
+      >
+        <div onClick={handleClick} className="cursor-pointer mt-4">
+          {sidebarToggle === true ? (
+            <div>
+              {sidebarToggle === true ? (
+                <div className="flex">
+                  <div className="">asttrolok</div>
 
-      {/* {routes.map((elem) => {
-        return <h5 style={{ marginTop: "2%" }}>{elem.identifier}</h5>;
-      })} */}
->>>>>>> 3e34ad6922b9a312be177c466be6a907301aa435
+                  <div className="flex justify-end items-right ml-5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+                  />
+                </svg>
+              )}
+            </div>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          )}
+        </div>
+
+        <div className="flex-1 mt-2 ">
+          {routes.map((elem) => {
+            {
+              return sidebarToggle === true ? (
+                <div className="flex justify-between">
+                  <h6 className="mt-10 cursor-pointer"> {elem.icon}</h6>
+                  <p
+                    onClick={() => handleOptionClick(elem.name)}
+                    className={`mt-8 cursor-pointer py-2 px-4 ${
+                      selectedOption === elem.name
+                        ? "bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                        : ""
+                    }`}
+                  >
+                    {elem.name}
+                  </p>
+                </div>
+              ) : (
+                <h6 className="sm:mt-8 hidden lg:mt-8 cursor-pointer">
+                  {elem.icon}
+                </h6>
+              );
+            }
+          })}
+        </div>
+      </div>
     </div>
   );
 };
